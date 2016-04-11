de7f92006da657a96c6743b1c62dc86b8ec855cb
alemosie <alexandra.siega@gmail.com>
2016-04-11 16:44:58 -0400

    push for Shmuel

--- get-data.rb
+++ get-data.rb
@@ -6,13 +6,17 @@ require 'google_places'
 
 class GooglePlacesAPI
   @@APIKey = ""
+
+  attr_reader :client
+
   def initialize
     @@APIKey = YAML.load_file('application.yml').values[0] # reads secret key from YAML
-    @client = GooglePlaces::Client.new(key) # creates Google Places object
+    @client = GooglePlaces::Client.new(@@APIKey) # creates Google Places object
   end
-
   # DATA!: https://maps.googleapis.com/maps/api/place/autocomplete/xml?input=Amoeba&key=APIKEY
   #RestClient
   #json
-
 end
+
+newPlace = GooglePlacesAPI.new
+# binding.pry
